78b77fe63ff3b061c286ccd5c1261ab728a3e16c
Emily Tang <emilytang@dhcp-172-31-129-161.mobile.uci.edu>
2026-03-02 16:49:46 -0800

    Changed App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Step 1: import your page  here:
+
 
 
 
 function App() {
+  <BrowserRouter>
+      <Routes>
+         
+         {/* ── Public Routes ── */}
 
+         {/* Step 2: Please add your routes here */}
+
+      </Routes>
+  </BrowserRouter>
 
   const [count, setCount] = useState(0)
-
   return (
     <>
       vite connection is working
@@ -18,3 +29,5 @@ function App() {
 }
 
 export default App
+
+
