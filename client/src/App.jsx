997d0c8d6fcbfa7a4325ba8776cf8705403653ff
Farin Soriano <barkiesaurus@gmail.com>
2026-03-03 14:19:19 -0800

    set up tailwind + frontend structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-//Step 1: import your page  here:
-
-
-
+// Step 1: Import your pages here
+import Home from './pages/Home'
 
 function App() {
-  <BrowserRouter>
+  return (
+    <BrowserRouter>
       <Routes>
-         
-         {/* ── Public Routes ── */}
+        {/* ── Public Routes ── */}
+        <Route path="/" element={<Home />} />
 
-         {/* Step 2: Please add your routes here */}
+
+        {/* Step 2: Add more routes here (e.g., /login, /profile) */}
 
       </Routes>
-  </BrowserRouter>
-
-  const [count, setCount] = useState(0)
-  return (
-    <>
-      vite connection is working
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
-
-
+export default App;
