3b3893d58d7ea21d24f1c6e9d47352799b159528
Mailee Mendoza Dizon <maileed@uci.edu>
2026-04-11 11:31:17 -0700

    Added log in and logout for routes

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -3,6 +3,7 @@ import { supabaseAdmin } from "../../config/supabaseAdmin.js"
 
 const router = express.Router();
 
+// sign up
 router.post("/register", async(req, res) => {
     try {
         const { email, password, first_name, last_name, role } = req.body
@@ -42,8 +43,52 @@ router.post("/register", async(req, res) => {
 
     } catch (error) {
         console.error("Error: ", error)
-        res.status(500).json({ message: "Internal server error" })
+        res.status(500).json({ error: "Internal server error" })
+    }
+})
+
+// log in
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "Need email and password" })
+        }
+
+        const { error } = await supabaseAdmin.auth.signInWithPassword({
+            email,
+            password
+        })
+
+        if (error) {
+            console.error("Error: ", error.message)
+            res.status(500).json({ message: "Error logging in" })
+        }
+
+        res.status(200).json({ message: "Logged in" })
+    } catch (error) {
+        console.error("Error: ", error.message)
+        res.status(500).json({ error: "Internal server error"})
     }
 })
 
 export default router;
+
+// logout
+router.post("/logout", async (req, res) => {
+    try {
+        const { error } = await supabaseAdmin.auth.signOut();
+
+        if ( error ) {
+            console.error("Error: ", error.message)
+            res.status(500).json({ message: "Error Logging out" })
+        }
+
+        res.status(200).json({ message: "Log out successful" })
+
+    } catch (error) {
+        console.error("Error: ", error.message)
+        res.status(500).json({ error: "Internal server error" })
+    }
+})
