cb3239a72e3be6bbaea868b25ad11bc7b9cda18c
Mailee Mendoza Dizon <maileed@uci.edu>
2026-04-15 19:21:28 -0700

    Added signup backend and integrated login and signup to frontend

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // sign up
 router.post("/register", async(req, res) => {
     try {
-        const { email, password, first_name, last_name, role } = req.body
+        const { email, password, first_name, last_name, role, businessName, phoneNumber, org } = req.body
 
         const { data, error } = await supabaseAdmin.auth.signUp({
             email,
@@ -19,11 +19,12 @@ router.post("/register", async(req, res) => {
 
         const user = data.user
 
-        if ( !user?.id, !email, !first_name, !last_name, !role ) {
+        if ( !user?.id || !email || !first_name || !last_name || !role ) {
             return res.status(400).json({ message: "Need userId, first name, last name, and role" })
         }
 
-        const { error: insertError } = await supabaseAdmin
+        // inserting into the users table
+        const { error: userError } = await supabaseAdmin
             .from("users")
             .insert([
                 {
@@ -35,15 +36,82 @@ router.post("/register", async(req, res) => {
                 }
             ])
 
-        if (insertError) {
-            return res.status(500).json({ message: insertError.message })
+        if (userError) {
+            console.log("Error signing up")
+            return res.status(500).json({ message: userError.message })
         }
 
-        res.status(200).json({ message: "User uploaded successfully." })
+            // adding org to groups table
+            if (role === "org_member") {
+                if (!org) {
+                    return res.status(400).json({ message: "Org name is needed" })
+                }
+
+                const { data: orgData, error: orgError } = await supabaseAdmin
+                    .from("groups")
+                    .insert([
+                        {
+                            name: org,
+                            entity_type: "organization",
+                            created_by: user.id
+                        }
+                    ])
+                    .select()
+                    .single()
+
+                if (orgError) {
+                    console.log("Error adding org to group table")
+                    return res.status(500).json({ message: orgError.message })
+                }   
+
+                // adding org to membership table
+                if (orgData) {
+                    const { error: memberError } = await supabaseAdmin
+                        .from("memberships")
+                        .insert([
+                            {
+                                user_id: user.id,
+                                group_id: orgData.id
+                            }
+                        ])
+                    
+                    if (memberError) {
+                        console.log("Error adding org to membership table")
+                        return res.status(500).json({ message: memberError.message })
+                    }
+                }
+
+                return res.status(200).json({ message: "Organization uploaded successfully" })
+            } else if (role === "business") {
+                // adding business to groups table
+                if (!businessName || !phoneNumber) {
+                    return res.status(400).json({ message: "Need business name and phone number" })
+                }
+
+                const { error: busError } = await supabaseAdmin
+                    .from("groups")
+                    .insert([
+                        {
+                            name: businessName,
+                            entity_type: "business",
+                            phone_number: phoneNumber,
+                        }
+                    ])
+                
+                if (busError) {
+                    console.log("Error adding business to groups table")
+                    return res.status(500).json({ message: busError.message })
+                }
+
+                return res.status(200).json({ message: "Business uploaded successfully." })
+            }
+        
+
+        return res.status(200).json({ message: "User uploaded successfully." })
 
     } catch (error) {
         console.error("Error: ", error)
-        res.status(500).json({ error: "Internal server error" })
+        return res.status(500).json({ error: "Internal server error" })
     }
 })
 
