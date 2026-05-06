3354a89752c586f5c6f3448875ed5e524a1bc926
Julianna Alderete <jyaldere@uci.edu>
2026-05-06 01:11:34 -0700

    background for signup/login (animated?)

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,15 @@
 import SignupForm from "../components/features/SignupForm";
+import BigLogo from "../assets/samasama_logo2.svg";
+import AuthBackground from "../components/background/AuthBackground";
 
 export default function Signup() {
     return(
-            <div className="flex items-center justify-center h-screen bg-[#FFDCBE]">
+        <AuthBackground>
+            <div className="flex flex-col items-center justify-center h-screen">
+                <img src={BigLogo} alt="" className="w-auto h-36" />
+                
                 <SignupForm/>
             </div>
+        </AuthBackground>
         )
 }
