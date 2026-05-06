876263cf12e7838d0321cc225a20e00c740820ae
Julianna Alderete <jyaldere@uci.edu>
2026-05-06 00:33:38 -0700

    import header+svgs, add header to layout

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,16 +1,20 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import Header from "../../assets/Header.svg";
 
 export default function Layout() {
     return (
-        <div className="flex min-h-screen w-full">
-            <Sidebar />
-            
-            <main className="flex-grow flex flex-col items-center">
-                <div className="w-full">
-                    <Outlet />
-                </div>
-            </main>
+        <div className="flex flex-col min-h-screen w-full">
+            <img src={Header} alt="" className="w-full z-50 block" />
+            <div className="flex flex-1 w-fullbg-[#FFDDBE]">
+                <Sidebar />
+                
+                <main className="flex-grow flex flex-col items-center">
+                    <div className="w-full">
+                        <Outlet />
+                    </div>
+                </main>
+            </div>
 
         </div>
     );
