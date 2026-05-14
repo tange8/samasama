52330361ccc58aedf24e4007f89fe0c6a7827c46
Julianna Alderete <jyaldere@uci.edu>
2026-05-13 17:21:04 -0700

    comment abt header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
+      {/* Header on all pages */}
         <div className="flex flex-col h-screen overflow-hidden">
           <img src={Header} alt="" className="w-full z-50 block" />
           <div className="flex-1 min-h-0">  
